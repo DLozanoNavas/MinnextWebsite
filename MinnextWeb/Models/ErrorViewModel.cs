9d363c8ae648b4590a0e82b3d7fd5da310db1b87
Daniel Fernando Lozano <cto@fenix-alliance.com>
2018-09-03 23:42:12 -0500

    [Adds] Initial MVC project

---
+++ MinnextWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MinnextWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
